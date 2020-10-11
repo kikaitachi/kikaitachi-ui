8240a16c0a093dfdeb00abd7b703f5d793aacc4e
Mykolas Juraitis <mykolas.juraitis@sg-tech.co.uk>
2020-10-11 22:18:02 +0100

    Only continue building if previous command didn't fail

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-deno bundle src/main.js | minify --mime text/javascript > web/main.js
-
-minify src/main.css > web/main.css
-
-cat src/index.html | sed -e '/<!-- css -->/{r web/main.css' -e 'd}' | sed -e '/<!-- js -->/{r web/main.js' -e 'd}' > web/index.html
-
-rm web/main.js web/main.css
+deno bundle src/main.js | minify --mime text/javascript > web/main.js && \
+  minify src/main.css > web/main.css && \
+  cat src/index.html | sed -e '/<!-- css -->/{r web/main.css' -e 'd}' | \
+  sed -e '/<!-- js -->/{r web/main.js' -e 'd}' > web/index.html && \
+  rm web/main.js web/main.css
