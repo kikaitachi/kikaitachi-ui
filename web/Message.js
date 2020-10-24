752882d741f7d2d709be5d20a5a4b81ef9145ee0
Mykolas Juraitis <mykolas.juraitis@sg-tech.co.uk>
2020-10-25 00:00:41 +0100

    Fix reading of int values

--- web/Message.js
+++ web/Message.js
@@ -8,9 +8,11 @@ export class Message {
   getUnsignedInt() {
     let byte = this.dataView.getInt8(this.index++);
     let value = byte & 127;
+    let shift = 7;
     while ((byte & (1 << 7)) != 0) {
       byte = this.dataView.getInt8(this.index++);
-      value = (value << 7) + (byte & 127);
+      value = value + ((byte & 127) << shift);
+      shift += 7;
     }
     return value;
   }
@@ -19,9 +21,11 @@ export class Message {
     let byte = this.dataView.getInt8(this.index++);
     let sign = (byte & (1 << 6)) != 0 ? -1 : 1;
     let value = byte & 63;
+    let shift = 6;
     while ((byte & (1 << 7)) != 0) {
       byte = this.dataView.getInt8(this.index++);
-      value = (value << 7) + (byte & 127);
+      value = value + ((byte & 127) << shift);
+      shift += 7;
     }
     return value * sign;
   }
