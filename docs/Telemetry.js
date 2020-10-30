c01c4f04d8e2d60cf3b4bcee53386f799ba324a4
Mykolas Juraitis <mykolas.juraitis@sg-tech.co.uk>
2020-10-30 14:12:56 +0000

    Support commands via shortcuts

--- docs/Telemetry.js
+++ docs/Telemetry.js
@@ -1,6 +1,6 @@
 const TELEMETRY_TYPE_INT = 0;
 const TELEMETRY_TYPE_STRING = 1;
-const TELEMETRY_TYPE_ACTION = 2;
+const TELEMETRY_TYPE_COMMAND = 2;
 const TELEMETRY_TYPE_STL = 3;
 
 export class Telemetry {
@@ -18,6 +18,22 @@ export class Telemetry {
         this.#clickedShortcut = null;
       }
     });
+
+    document.addEventListener('keydown', event => {
+      this.#idToItem.forEach((key, value) => {
+        if (value.type == TELEMETRY_TYPE_COMMAND && value.value == event.code) {
+          this.#onTelemetryChanged(value.id, 1);
+        }
+      });
+    });
+
+    document.addEventListener('keyup', event => {
+      this.#idToItem.forEach(value => {
+        if (value.type == TELEMETRY_TYPE_COMMAND && value.value == event.code) {
+          this.#onTelemetryChanged(value.id, 0);
+        }
+      });
+    });
   }
 
   getContainer() {
@@ -32,7 +48,7 @@ export class Telemetry {
     if (type == TELEMETRY_TYPE_INT) {
       return msg.readSignedInt();
     }
-    if (type == TELEMETRY_TYPE_STRING || type == TELEMETRY_TYPE_ACTION) {
+    if (type == TELEMETRY_TYPE_STRING || type == TELEMETRY_TYPE_COMMAND) {
       return msg.readString();
     }
     console.log('Unknown telemetry type: ' + type);
@@ -56,7 +72,7 @@ export class Telemetry {
     } else {
       this.getContainer().appendChild(itemContainer);
     }
-    if (type == TELEMETRY_TYPE_ACTION) {
+    if (type == TELEMETRY_TYPE_COMMAND) {
       item.innerHTML = '<span class="telemetryItemName" style="vertical-align: middle">' + name + '</span> <span class="telemetryItemShortcut" id="telemetryItemValue' + id + '">' + value + '</span>';
       const shortcut = document.getElementById('telemetryItemValue' + id);
       shortcut.addEventListener('mousedown', event => {
