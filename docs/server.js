39154d5e1e6e622f47b27d81809ecbb562f9c698
Mykolas Juraitis <61438920+kikaitachi@users.noreply.github.com>
2020-12-20 11:54:13 +0000

    Support URLs with parameters

--- docs/server.js
+++ docs/server.js
@@ -2,7 +2,12 @@ var fs = require('fs'),
     http = require('http');
 
 http.createServer(function (req, res) {
-  let fileName = __dirname + (req.url === '/' ? '/index.html' : req.url);
+  let url = req.url;
+  const index = url.indexOf('?');
+  if (index != -1) {
+    url = url.substr(0, index);
+  }
+  let fileName = __dirname + (url === '/' ? '/index.html' : url);
   if (!fs.existsSync(fileName) || fs.lstatSync(fileName).isDirectory()) {
     fileName += '.html';
   }
